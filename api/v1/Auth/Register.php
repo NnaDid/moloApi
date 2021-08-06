534e62151a273597bbada0c456b581741fdbd98a
NnaDid <chrispneuma@gmail.com>
2021-08-06 17:28:02 +0100

    Register Api

--- api/v1/Auth/Register.php
+++ api/v1/Auth/Register.php
@@ -13,20 +13,17 @@
     }
        
     private function register($input){
-        // `userId`, `fullName`, `email`, `phone`, `bvn`, `paswd`, `phone_status`, `email_status`, `createdAt`, `updateedAt`, `profilePix`
         $con    = $this->con();
         // decoding the received JSON and store into $obj variable.
         $obj    = json_decode($input,true); 
-        $fname  = $obj['fullName']; 
+        $fname  = $obj['name']; 
         $email  = $obj['email'];
         $phone  = $obj['phone'];
-        $pass   = $obj['paswd'];     
-        $bvn    = $obj['bvn'];  
+        $pass   = $obj['paswd'];   
     if($this->checkUserExist($email,"users","email")==false){	 
-        if($this->checkUserExist($phone,"users","phone")==false){
-            if($this->checkUserExist($bvn,"users","bvn")==false){
+        if($this->checkUserExist($phone,"users","phone")==false){ 
                 $pass_hash = password_hash($pass,PASSWORD_DEFAULT); 
-                $sql       = "INSERT INTO `users`(`fullName`,`email`,`phone`,`bvn`,`paswd`,`createdAt`) VALUES(?,?,?,?,?,NOW())";
+                $sql       = "INSERT INTO `users`(`name`,`email`,`phone`,`paswd`,`createdAt`) VALUES(?,?,?,?,NOW())";
                 $stmt      = $con->prepare($sql);
                 $stmt->bind_param("sssis",$fname,$email,$phone,$bvn,$pass_hash);
                 if($stmt->execute()){
@@ -34,10 +31,7 @@
                 }else{
                     $this->data['message'] = "Error Registering ".$stmt->error;
                 }
-               $stmt->close();
-            }else{
-                $this->data['message'] = "User with already exist with this BVN";
-            }
+               $stmt->close(); 
         }else{
             $this->data['message'] = "User with this phone number already exist";
         }
