86943f6e66e7e8a0e5d1caae5b2934ca1de15109
NnaDid <chrispneuma@gmail.com>
2021-08-08 11:19:55 +0100

    Latest 3

--- api/v1/Common.php
+++ api/v1/Common.php
@@ -26,35 +26,54 @@ trait Common{
         return  $queryRow;
     }
 
-        // Get User/member details by UserName
-    public function getMemberByUserName($uName){
-        $con        = $this->con();
-        $queryRow   = $con->query("SELECT * FROM `users` WHERE `userName` ='$uName'")->fetch_assoc();
-        return  $queryRow;
-     }
-        
+    public function getUserByPhoneNumber($phone){
+        $con  = $this->con();
+        $sql  = "SELECT * FROM `users` WHERE `phone`=?";
+        $stmt = $con->prepare($sql);
+        $stmt->bind_param("s",$phone);
+        if($stmt->execute()){
+            $res = $stmt->get_result();
+            $row = $res->fetch_assoc();
+            return $row;
+        }
+    }
+         
 
     public function checkUserExist($val,$table,$col){
         $con     = $this->con();
        if(!empty($val)){
-       $sql       = "SELECT * FROM `".$table."` WHERE `".$col."`=?";
-       $stmt      = $con->prepare($sql);
-       $stmt->bind_param("s",$val);
-       $exec      = $stmt->execute();
+        $sql       = "SELECT * FROM `".$table."` WHERE `".$col."`=?";
+        $stmt      = $con->prepare($sql);
+        $stmt->bind_param("s",$val);
+        $exec      = $stmt->execute();
        if($exec){
-       $result   = $stmt->get_result();
-       $num_rows = $result->num_rows;
-       if($num_rows>0){
-           return true;
-       }else{
-           return false;
-       }
+            $result   = $stmt->get_result();
+            $num_rows = $result->num_rows;
+            if($num_rows>0){
+                return true;
+            }else{
+                return false;
+            }
        $stmt->close();
-       }
-       }
+        }
+      }
    }
 
 
+	 // This function creates Transaction Record and returns true on success and false on failure
+     public function performTxn($uid,$txType,$amount,$txRef){
+        $con     = $this->con();
+        $sql    = "INSERT INTO `m_transactions`(`userId`,`amount`,`txtTpe`,`txRef`,`createdAt`) VALUES(?,?,?,?,NOW())";
+        $stmt     = $con->prepare($sql);
+        $stmt->bind_param("isss",$uid,$amount,$txType,$txRef);
+        if($stmt->execute()){
+           return true;
+        }else{
+           return false;
+        }
+        $stmt->close();
+        $con->close();
+    }
 
 
 }
